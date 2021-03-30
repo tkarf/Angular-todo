c13b51eec220ec9f3ed4edf824bb602f516c9dd5
tkarf <53835252+tkarf@users.noreply.github.com>
2021-03-30 13:51:51 +0300

    Angular - Todoapp

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+
+  constructor() {}
+
+  //Methods = functions within class
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: 'First todo',
+        completed: false,
+      },
+      {
+        content: 'Second todo',
+        completed: true,
+      },
+    ];
+  }
+}
