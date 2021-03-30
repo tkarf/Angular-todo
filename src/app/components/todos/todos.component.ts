ab7d558bb867a6e85a2bbfc6ec98cd261eda9e2d
tkarf <53835252+tkarf@users.noreply.github.com>
2021-03-30 14:18:02 +0300

    Todoapp

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -8,6 +8,7 @@ import { Todo } from '../../models/Todo';
 })
 export class TodosComponent implements OnInit {
   todos: Todo[];
+  inputTodo: string = '';
 
   constructor() {}
 
@@ -24,5 +25,26 @@ export class TodosComponent implements OnInit {
         completed: true,
       },
     ];
+  }
+  //toggleDone():void (returns null), toggleDobe():string/number etc -> return "string"
+  toggleDone(id: number) {
+    //.this = liknande som React class componenter och bindings.
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+      return v;
+    });
+  }
+  //Id has to be a number - Typescript
+  deleteTodo(id: number) {
+    //Returns everything that is not equal to id
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addToDo() {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false,
+    });
+    this.inputTodo = '';
   }
 }
